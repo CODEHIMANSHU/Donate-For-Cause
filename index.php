906db4a349c9639585af671e1bec0f8a3a6bef2a
HIMANSHU AGRAWAL <himanshuagrawal1998@gmail.com>
2016-03-18 05:23:48 +0530

    Added files via upload
    Front end of index and register created

--- index.php
+++ index.php
@@ -1,53 +1,95 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <!--for title icon-->
+    <link rel="shortcut icon" href="img/title.ico" />
     <title>
       Donate For Cause
     </title>
   </head>
-  <body>
-
-  <!--registration page link-->
-  <a href="register.php">Register</a>
-  <!--login interface to ask email and password-->
-      
-    <form action="" method="post">
-      Email:<br>
-      <input type="email" name="emai"><br>
-      Password:<br>
-      <input type="password" name="pass"><br>
-      <input type="submit" value="Submit" name="submit">
-    </form>
-    <?php
-      //after entering details and pressing submit
-      if(isset($_POST["submit"]))
-      {
-        //connectiong to mysql and database test
-        $link = mysql_connect('localhost', 'root', '');
-        $mydb = mysql_select_db('test',$link);  
-        //storing the input values in variables   
-        $emai = $_POST["emai"];
-        $pass = $_POST["pass"];
-        //reading email and password from userinfo and checking login credentials
-        $sql = "SELECT * FROM userinfo WHERE (emai= '$emai' AND pass='$pass')";
-        $data = mysql_query($sql);
-        $count= mysql_num_rows($data);
-        $row=mysql_fetch_assoc($data);
-        //row exists only if the existing email pass match
-        if($row)
+  <body background="img/world.png" class="grey darken-1 white-text">
+    <!-- Dropdown Structure for login-->
+    <ul id="d1" class="dropdown-content" style="padding:10px 10px 10px 10px;">
+      <li>      
+      <!--login interface to ask email and password-->
+      <form action="" method="post">
+        <b>Email:</b><br>
+        <input type="email" name="emai"><br>
+        <b>Password:</b><br>
+        <input type="password" name="pass"><br>
+        <center>
+          <button class="btn-large waves-effect waves-light red darken-4 " type="submit" name="submit">Login
+          <i class="tiny material-icons right" >send</i>
+          </button>
+        </center>
+      </form>
+      </li>
+    </ul>    
+    <!--navbar-->
+    <nav>
+      <div class="nav-wrapper red darken-4">
+        <a class="brand-logo" style="padding:0px 10px;">Donate For Cause</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <!-- Dropdown Trigger -->
+          <li><a class="dropdown-button waves-effect waves-light" href="#!" data-activates="d1" style="width:200px;">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li>
+          <li><a href="register.php" class="waves-effect waves-light">Sign Up</a></li>
+        </ul>
+      </div>
+    </nav>
+    <br><br>
+    <div class="container black" style="padding:0px 20px 0px 0px; border-radius:1em;">  
+      <br>
+      <?php
+        //after entering details and pressing submit
+        if(isset($_POST["submit"]))
         {
-          //to create cookies so that the user is logged in
-          setcookie("success","log", time() + (86400 * 30), "/");
-          setcookie("emai",$emai, time() + (86400 * 30), "/");
-          //redirecting user to dashboard if login successful
-          header("Location: http://localhost/DonateForCause/dashboard.php");
+          //connectiong to mysql and database test
+          $link = mysql_connect('localhost', 'root', '');
+          $mydb = mysql_select_db('test',$link);  
+          //storing the input values in variables   
+          $emai = $_POST["emai"];
+          $pass = $_POST["pass"];
+          //reading email and password from userinfo and checking login credentials
+          $sql = "SELECT * FROM userinfo WHERE (emai= '$emai' AND pass='$pass')";
+          $data = mysql_query($sql);
+          $count= mysql_num_rows($data);
+          $row=mysql_fetch_assoc($data);
+          //row exists only if the existing email pass match
+          if($row)
+          {
+            //to create cookies so that the user is logged in
+            setcookie("success","log", time() + (86400 * 30), "/");
+            setcookie("emai",$emai, time() + (86400 * 30), "/");
+            //redirecting user to dashboard if login successful
+            header("Location: http://localhost/DonateForCause/dashboard.php");
+          }
+          //if there doesn't exit the email pass combination
+          else
+          {
+            echo '<div class="right">Login Failed</div>';
+          }
         }
-        //if there doesn't exit the email pass combination
-        else
-        {
-          echo "Login Failed";
-        }
-      }
-    ?>
+      ?>
+      <br><br><br>
+      <!--image slider-->
+      <div class="carousel">
+        <a class="carousel-item" href="#one!"><img src="img/pic4.jpg" width=300px; height=250px;></a>
+        <a class="carousel-item" href="#two!"><img src="img/pic1.jpg" width=300px; height=250px;></a>
+        <a class="carousel-item" href="#three!"><img src="img/pic6.jpg" width=300px; height=250px;></a>
+        <a class="carousel-item" href="#four!"><img src="img/pic3.jpg" width=300px; height=250px;></a>
+        <a class="carousel-item" href="#five!"><img src="img/pic5.jpg" width=300px; height=250px;></a>
+      </div>
+      <br><br>
+    </div>
+    <!--Import jQuery before materialize.js-->
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="js/materialize.js"></script>
+    <script src="js/init.js"></script>
   </body>
 </html>
--- register.php
+++ register.php
@@ -1,8 +1,32 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <!--for title icon-->
+    <link rel="shortcut icon" href="img/title.ico" />
+    <title>
+      Donate For Cause
+    </title>
   </head>
-  <body>
+  <body background="img/regbg1.png" class="black">
+    <!--navbar-->
+    <nav>
+      <div class="nav-wrapper red darken-4">
+        <a class="brand-logo" style="padding:0px 10px;">Donate For Cause</a>
+      </div>
+    </nav>
+    <br><br><br>
+    <div class="container" style="padding:20px 40px 20px 40px; border-radius: 2em 2em 2em 2em;">
+      <div class="row">
+        <div class="col s12 m2">. </div>
+        <div class="col sl2 m8 white" style="padding:20px 40px 20px 40px; border-radius: 2em 2em 2em 2em;">
+ 
+
 
 <!--connectiong to mysql and database test-->
           <?php
@@ -16,14 +40,23 @@
             Name:<br>
             <input type="text" name="name" required><br>
             Blood Group:<br>
-            <input type="radio" name="bloo" value="1" checked> AB+<br>
-            <input type="radio" name="bloo" value="2" checked> AB-<br>
-            <input type="radio" name="bloo" value="3"> A+<br>
-            <input type="radio" name="bloo" value="4"> A-<br>
-            <input type="radio" name="bloo" value="5"> B+<br>
-            <input type="radio" name="bloo" value="6"> B-<br>
-            <input type="radio" name="bloo" value="7"> O+<br>
-            <input type="radio" name="bloo" value="8"> O-<br>
+              <input name="bloo" type="radio" id="test1" />
+                <label for="test1">AB+&nbsp;&nbsp;&nbsp;</label>
+              <input name="bloo" type="radio" id="test2" />
+                <label for="test2">AB-&nbsp;&nbsp;&nbsp;</label>
+              <input name="bloo" type="radio" id="test3" />
+                <label for="test3">A-&nbsp;&nbsp;&nbsp;</label>
+              <input name="bloo" type="radio" id="test4" />
+                <label for="test4">B+&nbsp;&nbsp;&nbsp;</label>
+              <input name="bloo" type="radio" id="test5" />
+                <label for="test5">B-&nbsp;&nbsp;&nbsp;</label>
+              <input name="bloo" type="radio" id="test6" />
+                <label for="test6">O+&nbsp;&nbsp;&nbsp;</label>
+              <input name="bloo" type="radio" id="test7" />
+                <label for="test7">O-&nbsp;&nbsp;&nbsp;</label>
+                <br><br>
+             
+            
             Email Id:<br>
             <input type="email" name="emai" required><br>
             Mobile No.:<br>
@@ -33,11 +66,19 @@
             Password:<br>
             <input type="password" name="pass" required><br>
             Are you a regular Donor:<br>
-            <input type="radio" name="acti" value="1" checked> ALWAYS READY<br>
-            <input type="radio" name="acti" value="0">NOT NOW<br>
-            <input type="submit" value="Register" name="submit">
+            <input name="acti" type="radio" id="test8" />
+              <label for="test8">ALWAYS READY</label>
+            <input name="acti" type="radio" id="test9" />
+              <label for="test9">NOT NOW</label>
+              <br><br>
+             <div class="center"><button class="btn waves-effect waves-light" type="submit" name="submit">Submit
+                <i class="material-icons right"></i>
+              </button></div>
           </form>
         </div>
+        </div>
+        </div>
+        </div> 
          <?php
          if(isset($_POST["submit"]))
          {
@@ -72,6 +113,10 @@
            }
           }
           ?>
+    <!--Import jQuery before materialize.js-->
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="js/materialize.js"></script>
+    <script src="js/init.js"></script>
 
 </body>
 </html>
